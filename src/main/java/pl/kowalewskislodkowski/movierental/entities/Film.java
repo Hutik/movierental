9621f2620af0bc493ab0e048927c209d822f4098
Hutik <mariusz10081997@gmail.com>
2022-04-19 18:26:34 +0200

    Film and Category Entities Creating (+Trace form db added to gitignore )

--- src/main/java/pl/kowalewskislodkowski/movierental/entities/Film.java
+++ src/main/java/pl/kowalewskislodkowski/movierental/entities/Film.java
@@ -1,12 +1,59 @@
 package pl.kowalewskislodkowski.movierental.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Films")
+@Table
 public class Film {
     @Id
     Integer id;
+    String title;
+    String description;
+    Float price;
+    @ManyToMany
+    Set<Category> categories = new HashSet<>();
+    boolean newest;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Float getPrice() {
+        return price;
+    }
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+    public Set<Category> getCategories() {
+        return categories;
+    }
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+    public boolean isNewest() {
+        return newest;
+    }
+    public void setNewest(boolean newest) {
+        this.newest = newest;
+    }
 }
